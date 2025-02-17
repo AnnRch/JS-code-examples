f044677a5773702e5c590ce9c5c3f13e458f675c
AnnRch <132267569+AnnRch@users.noreply.github.com>
2025-02-17 08:41:52 +0200

    html, css, js files for web page design task added

---
+++ main.js
@@ -0,0 +1,448 @@
+
+var store1 = [
+    {
+        
+        "fruit": [
+            {
+                "name": "banana",
+                "price": 3.45,
+                "photo": "images/bananas.jpeg"
+            },
+            {
+                "name": "apple",
+                "price": 3.25,
+                "photo": "images/apples.jpeg"
+            },
+            {
+                "name": "orange",
+                "price":  3.95,
+                "photo":"images/oranges.jpeg"
+            },
+            {
+                "name": "kiwi",
+                "price": 7.50,
+                "photo":"images/kiwi.jpeg"
+            }
+        ]
+    },
+
+    {
+        "vegetables": [
+            {
+                "name":"tomato cherry",
+                "price": 7.00,
+                "photo":"images/tomato_cherry.jpeg"
+            },
+            {
+                "name": "cucumber",
+                "price": 5.50,
+                "photo": "images/cucumbers.jpg"
+            },
+            {
+                "name":"pepper",
+                "price": 8.95,
+                "photo":"images/peppers.jpeg"
+            },
+            {
+                "name":"potato",
+                "price": 1.90,
+                "photo":"images/potato.jpeg"
+            },
+            {
+                "name":"onion",
+                "price": 2.45,
+                "photo":"images/onion.jpeg"
+            }
+        ]
+    },
+    {
+        "cereals and pasta":[
+            {
+                "name":"rice",
+                "price": 3.90,
+                "photo":"images/rice.jpeg"
+            },
+            {
+                "name":"spagetti",
+                "price": 4.99,
+                "photo":"images/spagetti.jpeg"
+            },
+            {
+                "name":"buckwheat",
+                "price": 3.99,
+                "photo":"images/buckweat.jpeg"
+            },
+            {
+                "name":"pearl barley",
+                "price": 3.50,
+                "photo":"images/pearl_barley.jpeg"
+            }
+        ]
+    },
+    {
+        "drinks":[
+            {
+                "name":"fuse tea / 1L",
+                "price": 2.550,
+                "photo":"images/fuse_tea.jpeg"
+            },
+            {
+                "name":"cola sugar free / 1L",
+                "price": 2.50,
+                "photo":"images/cola_sugar_free.jpeg"
+            },
+            {
+                "name":"sandora juice / 1L",
+                "price": 5.50,
+                "photo":"images/sandora.jpeg"
+            },
+            {
+                "name":"lemonade / 0.8L",
+                "price": 2.80,
+                "photo":"images/lemonade.jpeg"
+            }
+        ]
+    }
+];
+
+var store2 = [
+    {
+        
+        "fruit": [
+            {
+                "name": "banana",
+                "price": 5.45,
+                "photo": "images/bananas.jpeg"
+            },
+            {
+                "name": "apple",
+                "price": 4.40,
+                "photo": "images/apples.jpeg"
+            },
+            {
+                "name": "orange",
+                "price":  2.95,
+                "photo":"images/oranges.jpeg"
+            },
+            {
+                "name": "kiwi",
+                "price": 9.20,
+                "photo":"images/kiwi.jpeg"
+            }
+        ]
+    },
+
+    {
+        "vegetables": [
+            {
+                "name":"tomato cherry",
+                "price": 7.50,
+                "photo":"images/tomato_cherry.jpeg"
+            },
+            {
+                "name": "cucumber",
+                "price": 4.50,
+                "photo": "images/cucumbers.jpg"
+            },
+            {
+                "name":"pepper",
+                "price": 8.80,
+                "photo":"images/peppers.jpeg"
+            },
+            {
+                "name":"potato",
+                "price": 1.80,
+                "photo":"images/potato.jpeg"
+            },
+            {
+                "name":"onion",
+                "price": 2.35,
+                "photo":"images/onion.jpeg"
+            }
+        ]
+    },
+    {
+        "cereals and pasta":[
+            {
+                "name":"rice",
+                "price": 5.90,
+                "photo":"images/rice.jpeg"
+            },
+            {
+                "name":"spagetti",
+                "price": 5.99,
+                "photo":"images/spagetti.jpeg"
+            },
+            {
+                "name":"buckwheat",
+                "price": 5.50,
+                "photo":"images/buckweat.jpeg"
+            },
+            {
+                "name":"pearl barley",
+                "price": 2.50,
+                "photo":"images/pearl_barley.jpeg"
+            }
+        ]
+    },
+    {
+        "drinks":[
+            {
+                "name":"fuse tea / 1L",
+                "price": 2.60,
+                "photo":"images/fuse_tea.jpeg"
+            },
+            {
+                "name":"cola sugar free / 1L",
+                "price": 2.60,
+                "photo":"images/cola_sugar_free.jpeg"
+            },
+            {
+                "name":"sandora juice / 1L",
+                "price": 5.99,
+                "photo":"images/sandora.jpeg"
+            },
+            {
+                "name":"lemonade / 0.8L",
+                "price": 2.90,
+                "photo":"images/lemonade.jpeg"
+            }
+        ]
+    }
+];
+
+
+var store3 = [
+    {
+        
+        "fruit": [
+            {
+                "name": "banana",
+                "price": 3.99,
+                "photo": "images/bananas.jpeg"
+            },
+            {
+                "name": "apple",
+                "price": 2.8,
+                "photo": "images/apples.jpeg"
+            },
+            {
+                "name": "orange",
+                "price":  2.95,
+                "photo":"images/oranges.jpeg"
+            },
+            {
+                "name": "kiwi",
+                "price": 8,
+                "photo":"images/kiwi.jpeg"
+            }
+        ]
+    },
+
+    {
+        "vegetables": [
+            {
+                "name":"tomato cherry",
+                "price": 6.00,
+                "photo":"images/tomato_cherry.jpeg"
+            },
+            {
+                "name": "cucumber",
+                "price": 4.50,
+                "photo": "images/cucumbers.jpg"
+            },
+            {
+                "name":"pepper",
+                "price": 9,
+                "photo":"images/peppers.jpeg"
+            },
+            {
+                "name":"potato",
+                "price": 1.60,
+                "photo":"images/potato.jpeg"
+            },
+            {
+                "name":"onion",
+                "price": 2.99,
+                "photo":"images/onion.jpeg"
+            }
+        ]
+    },
+    {
+        "cereals and pasta":[
+            {
+                "name":"rice",
+                "price": 4.99,
+                "photo":"images/rice.jpeg"
+            },
+            {
+                "name":"spagetti",
+                "price": 3.80,
+                "photo":"images/spagetti.jpeg"
+            },
+            {
+                "name":"buckwheat",
+                "price": 5.50,
+                "photo":"images/buckweat.jpeg"
+            },
+            {
+                "name":"pearl barley",
+                "price": 2.50,
+                "photo":"images/pearl_barley.jpeg"
+            }
+        ]
+    },
+    {
+        "drinks":[
+            {
+                "name":"fuse tea / 1L",
+                "price": 2.55,
+                "photo":"images/fuse_tea.jpeg"
+            },
+            {
+                "name":"cola sugar free / 1L",
+                "price": 2.80,
+                "photo":"images/cola_sugar_free.jpeg"
+            },
+            {
+                "name":"sandora juice / 1L",
+                "price": 5.99,
+                "photo":"images/sandora.jpeg"
+            },
+            {
+                "name":"lemonade / 0.8L",
+                "price": 2.90,
+                "photo":"images/lemonade.jpeg"
+            }
+        ]
+    }
+];
+
+
+// var loadButton = document.getElementById("load-button");
+let categories = document.getElementById("category");
+
+function changeStore(){
+
+    let categories = document.getElementById("category");
+    categories.options.length = 1;
+
+    let select = document.getElementById("store");
+    document.getElementById("store-span").innerHTML = select.options[select.selectedIndex].text;
+    let value = select.options[select.selectedIndex].value;
+    if(value == "store1")
+            LoadData(store1)
+        else if(value == "store2")
+            LoadData(store2)
+        else 
+            LoadData(store3);
+}
+
+    function LoadData(items){
+        console.log(items);
+        for (let index in items){
+            var option = document.createElement("option");
+             option.text = Object.keys(items[index]);
+             option.value = 
+                document.getElementById("store")
+                    .options[document.getElementById("store").selectedIndex]
+                    .value;
+             categories.add(option);
+         }
+    }
+    
+
+function getIndexBykey(key, assoc_array){
+    let items = assoc_array == "store1"
+                ? store1
+                : assoc_array == "store2"
+                ? store2
+                : store3;
+    for(let index in items){
+        if (Object.keys(items[index]) == key){
+            return index;
+        }
+    }
+}
+
+function changeSelect(idSelect1, idSelect2, array){
+
+    document.getElementById("image").src = "Images/placeholder.png";
+    let select1 = document.getElementById(idSelect1);
+    let key = select1.options[select1.selectedIndex].text;  
+    
+
+    let index = array == "store1" 
+                ? getIndexBykey(key, store1)
+                : array == "store2"
+                ? getIndexBykey(key, store2)
+                : getIndexBykey(key, store3)
+            ;
+    let values = array == "store1"
+                ? store1[index][key]
+                : array == "store2"
+                ? store2[index][key]
+                : store3[index][key];
+
+
+    let select2 = document.getElementById(idSelect2);
+   
+    if (select2.options.length == 1){
+        createOptions(idSelect2, key, values);
+
+    }
+    else {
+        ClearOptions(idSelect2);
+        createOptions(idSelect2, key, values);
+    }   
+        
+}
+
+function createOptions(selectId, key, values){
+    for (let value of values){
+        var option = document.createElement("option");
+        option.value = key;
+        option.text = value.name;
+        document.getElementById(selectId).add(option);
+    }
+}
+
+function changeCategory(){
+    let store = document.getElementById("store");
+    changeSelect("category","items", store.options[store.selectedIndex].value);
+}
+
+function changeItem(){
+    let select_item = document.getElementById("items"); 
+    let value = select_item.options[select_item.selectedIndex].text;
+    let key = select_item.options[select_item.selectedIndex].value;
+    let header = document.getElementById("name");
+    header.innerHTML = "Product: " + value;
+
+    let store = document.getElementById("store");
+    let store_value = store.options[store.selectedIndex].value;
+    let index = getIndexBykey(key, store_value);
+
+    console.log("store_value = " + store_value);
+    let item = store_value == "store1"
+                 ? getItemByName(value, store1[index][key])
+                 : store_value == "store2"
+                 ? getItemByName(value, store2[index][key])
+                 : getItemByName(value, store3[index][key]);   
+                
+    document.getElementById("item-price").innerHTML ="Price: " + item.price + " gel";
+    document.getElementById("image").src = item.photo;
+}
+
+function getItemByName(name, array){
+    for(let item of array){
+        if(item.name == name)
+            return item;
+    }
+}
+
+function ClearOptions(id)
+{
+	document.getElementById(id).options.length = 1;
+
+}
